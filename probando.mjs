c2f6b3d624b843b346f6929884938df62ce883c1
Soy Josef <josefesquino@gmail.com>
2025-10-28 11:33:35 -0500

    Estoy cambiando el console.log

--- probando.mjs
+++ probando.mjs
@@ -10,7 +10,7 @@ const app = express()
 mongoose.connect('mongodb://josef:password@monguito:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-console.log('listand...')
+console.log('listando...')
 const animales = await Animal.find();
 return res.send(animales)
 })
